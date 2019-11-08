9420005ff271fd1211e676fd176074898fd505c9
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2019-11-07 23:42:56 -0600

    Get Data from the Github API and Update State

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: {},
+    active: false
+  }
 
   handletToggle = (event) => {
     console.log("button clicked");
+    fetch("https://api.github.com/users/thurt")
+    .then(res => res.json())
+    .then(user => {
+      this.setState({ user })
+    });
   };
 
   render() {
